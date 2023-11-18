8ccfa850677ca528c1c7f07a7ee8476463f973bd
Valia Fetisov <contact@valiafetisov.com>
2023-11-18 20:40:16 +0300

    fix production build

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,3 +1,5 @@
+const define = process.env.NODE_ENV === 'production' ? {} : { global: 'window' }
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
@@ -5,8 +7,6 @@ export default defineNuxtConfig({
   css: ['~/assets/css/preflight.css'],
   modules: ['@nuxtjs/tailwindcss', '@bg-dev/nuxt-naiveui'],
   vite: {
-    define: {
-      global: "window",
-    }
+    define,
   }
 })
