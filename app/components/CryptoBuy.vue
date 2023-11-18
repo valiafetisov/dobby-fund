201b3497cad70bf010abeb6b20cbd221de55dc76
“DaeunYoon” <daa3230@gmail.com>
2023-11-18 18:08:02 +0300

    wip: working with iframe without getting result

--- app/components/CryptoBuy.vue
+++ app/components/CryptoBuy.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { NCollapseItem, NIcon, NButton, NSelect } from 'naive-ui';
 import { computed, ref } from 'vue';
-import type { Ref } from 'vue';
 import { Card as CardIcon } from '@vicons/ionicons5';
 import { format } from 'date-fns';
+
+const props = defineProps<{ accountAddress: string }>();
 
 const emits = defineEmits<{
   (
@@ -21,8 +22,31 @@ const options = [
 ];
 
 const currentBalance = ref(0);
+const showModal = ref(false);
 
 const title = computed(() => 'Fund Wallet');
+
+const widget = async () => {
+  const response = await fetch(
+    `https://api-sandbox.gatefi.com/onramp/v1/orders/${orderId}`,
+    {
+      method: 'GET',
+      redirect: 'follow',
+      headers: {
+        'access-control-allow-headers': 'Accept',
+        'api-key': 'eOLFHIEVQmwqJOAwWBOiFsfnNhncHigb',
+      },
+    }
+  );
+  const data = await response;
+  console.log(data);
+  return data.url;
+};
+
+const openModal = async () => {
+  showModal.value = true;
+  await widget();
+};
 </script>
 <template>
   <n-collapse-item name="1">
@@ -30,6 +54,11 @@ const title = computed(() => 'Fund Wallet');
       <n-icon><card-icon /></n-icon>&nbsp;
       <h4 class="font-semibold">{{ title }}</h4>
     </template>
+    <UnlimitBuyModal
+      :show="showModal"
+      :accountAddress="accountAddress"
+      @close="showModal = false"
+    />
     <div class="flex flex-col gap-y-2">
       <div>
         Fund the wallet by buying the selected crypto asset with your Euro
@@ -40,9 +69,12 @@ const title = computed(() => 'Fund Wallet');
         <n-select v-model:value="selectedToken" disabled :options="options" />
       </div>
       <div class="flex w-full gap-x-2">
-        <n-button type="primary">Buy {{ selectedToken }}</n-button>
+        <n-button ref="overlay-button" type="primary" @click="openModal"
+          >Buy {{ selectedToken }}</n-button
+        >
         <n-button disabled>Send {{ selectedToken }}</n-button>
       </div>
+
       <div class="flex gap-2">
         <span>Current balance: {{ currentBalance }} {{ selectedToken }}</span>
         <span class="text-gray-500"
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <h1>Index page</h1>
+    <n-collapse>
+      <CryptoBuy accountAddress="0xdC233b5368d39FED2EB99EE4dc225882D35ff4B6"
+    /></n-collapse>
   </div>
 </template>
