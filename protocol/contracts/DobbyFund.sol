0f73c9829ab09624846cb7c7272ac3514898fc12
Valia Fetisov <contact@valiafetisov.com>
2023-11-18 23:50:18 +0300

    update contract to emit some events

--- protocol/contracts/DobbyFund.sol
+++ protocol/contracts/DobbyFund.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.16;
 
-import "hardhat/console.sol";
-
 interface Erc20Like {
     function approve(address, uint256) external;
     function transferFrom(address, address, uint256) external;
@@ -21,10 +19,13 @@ contract DobbyFund {
 
     // Supported token
     Erc20Like public immutable token;
+    
     // Time window in seconds in which tokens are claimable after the claimableDate
     uint40 public constant claimableWindow = 10 * 365 days;
+    
     // If unclaimed after claimableDate + claimableWindow, funds can be transferred to donationDestination
     address public immutable donationDestination;
+    
     // Exchange router
     UniswapRouterLike public immutable router;
 
@@ -34,7 +35,11 @@ contract DobbyFund {
         uint256 amount;
     }
     mapping(address => Deposit[]) public deposits;
+
+    // Events
     event Deposited(address indexed receiver, uint40 indexed claimableDate, uint256 amount);
+    event Withdraw(address indexed receiver, uint256 amount);
+    event Donate(address indexed receiver, uint256 amount);
 
     constructor(address token_, address donationDestination_, address router_) {
         token = Erc20Like(token_);
@@ -45,6 +50,7 @@ contract DobbyFund {
     function deposit(address receiver, uint40 claimableDate, uint256 amount) external {
         token.transferFrom(msg.sender, address(this), amount);
         deposits[receiver].push(Deposit(claimableDate, amount));
+        emit Deposited(receiver, claimableDate, amount);
     }
 
     function depositEth(address receiver, uint40 claimableDate) external payable returns (uint256) {
@@ -53,6 +59,7 @@ contract DobbyFund {
         path[1] = address(token);
         uint256[] memory amounts = router.swapExactETHForTokens{ value: msg.value }(0, path, address(this), block.timestamp + 15);
         deposits[receiver].push(Deposit(claimableDate, amounts[1]));
+        emit Deposited(receiver, claimableDate, amounts[1]);
         return amounts[1];
     }
 
@@ -69,6 +76,7 @@ contract DobbyFund {
         }
         require(totalToTransfer > 0, 'nothing-to-withdraw');
         token.transferFrom(address(this), msg.sender, totalToTransfer);
+        emit Withdraw(msg.sender, totalToTransfer);
     }
 
     function donateUnclaimed(address receiver) external {
@@ -84,6 +92,7 @@ contract DobbyFund {
         }
         require(totalToTransfer > 0, 'nothing-to-donate');
         token.transferFrom(address(this), donationDestination, totalToTransfer);
+        emit Donate(receiver, totalToTransfer);
     }
 
     function balanceOf(address account) external view returns (uint256) {
