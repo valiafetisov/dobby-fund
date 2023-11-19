ea8d26eee1cb7338ccdeb88f5fe08ef0633113b5
Valia Fetisov <contact@valiafetisov.com>
2023-11-19 07:41:31 +0300

    fix

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -11,8 +11,8 @@
     </div>
     <div class="h-5 flex justify-center items-center">
       <span class="mr-2 h-full">Built for</span>
-      <nuxt-link class="text-primary underline" to="https://ethglobal.com/events/istanbul" target="_blank"
-        >ETHGlobal Istanbul <img src="~/assets/icons/head-earth.svg" class="w-6 h-6 inline-block"
+      <nuxt-link class="text-primary" to="https://ethglobal.com/events/istanbul" target="_blank"
+        ><span class="underline">ETHGlobal Istanbul</span> <img src="~/assets/icons/head-earth.svg" class="w-6 h-6 inline-block"
       /></nuxt-link>
     </div>
   </div>
