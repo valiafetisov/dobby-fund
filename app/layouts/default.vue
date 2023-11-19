8c463773e1b628073a8a72e2347f29bc728b83a8
DaeunYoon <34188262+DaeunYoon@users.noreply.github.com>
2023-11-19 07:41:52 +0300

    style: add globe on footer (#13)
    Co-authored-by: “DaeunYoon” <“daa3230”@gmail.com”>
    Co-authored-by: Valia Fetisov <contact@valiafetisov.com>

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,9 +9,11 @@
     <div class="grow">
       <slot />
     </div>
-    <div class="h-5 flex justify-center">
-      Built for&nbsp;
-      <nuxt-link class="text-primary underline" to="https://ethglobal.com/events/istanbul" target="_blank">ETHGlobal Istanbul</nuxt-link>
+    <div class="h-5 flex justify-center items-center">
+      <span class="mr-2 h-full">Built for</span>
+      <nuxt-link class="text-primary" to="https://ethglobal.com/events/istanbul" target="_blank"
+        ><span class="underline">ETHGlobal Istanbul</span> <img src="~/assets/icons/head-earth.svg" class="w-6 h-6 inline-block"
+      /></nuxt-link>
     </div>
   </div>
 </template>
